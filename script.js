14266a311a11e073dff7abc4047c7c544c74afd5
Olive <olivia.brantley9@gmail.com>
2022-02-14 11:12:51 -0800

    DOM works

--- script.js
+++ script.js
@@ -1,65 +1,121 @@
+///Variable and DOM ----------------------------------------
+
+
+
 let choices = ["rock", "paper", "scissors"];
 let computerScore = 0;
 let playerScore = 0;
 const rockButton = document.getElementById('rock');
 const paperButton = document.getElementById('paper');
 const sicssorsButton = document.getElementById('scissors');
+const choiceMade = document.getElementById('choice-made');
+const roundWinner = document.getElementById('round-winner');
+const gameWinner = document.getElementById('game-winner');
+const score = document.getElementById('score');
+const playAgain = document.getElementById ('play-again');
+const beats = {
+    paper:'rock',
+    scissors : 'paper',
+    rock:'scissors'
+};
 
-
-function clickListener(event) {
-    const playerSelection = event.target.id;
-    const computerSelection = computerPlay();
-    playRound(playerSelection, computerSelection);
-    console.log(`Player chose ${playerSelection}`);
-    console.log(`Computer chose ${computerSelection}`);
-    console.log(playRound(playerSelection, computerSelection));
-    
-}
 
 rockButton.addEventListener("click", clickListener);
 paperButton.addEventListener("click", clickListener);
 sicssorsButton.addEventListener("click", clickListener);
 
-   
+//buttons
+function clickListener(event) {
+    const playerSelection = event.target.id;
+    const computerSelection = computerPlay();
+    playRound(playerSelection, computerSelection);
+    finishedGame(); 
+}
+    // console.log(`Player chose ${playerSelection}`);
+    // console.log(`Computer chose ${computerSelection}`);
 
-//computer makes a choice
-    
+
+
+
+//GAME ---------------------------------------------------- 
+
+
+
+//computer makes a choice 
     function computerPlay() {
     
         let compRandom = Math.floor(Math.random() * choices.length);
         return choices[compRandom];
     }
     
-// round is played, 
+//round is played OPTIMIZED ------------------------------------
+function playRound(playerSelection, computerSelection) {
+    if (beats[playerSelection] === computerSelection ){
+        playerScore++;
+        choiceMade.textContent = `You chose ${playerSelection}. Computer chose ${computerSelection}`;
+        roundWinner.textContent = 'You Win!';
+        score.textContent = ` Your score: ${playerScore}. Computer Score ${computerScore}`;
+    }else if (beats[computerSelection] === playerSelection ) {
+        computerScore++;
+        choiceMade.textContent = `You chose ${playerSelection}. Computer chose ${computerSelection}`;
+        roundWinner.textContent = 'Computer Wins!';
+        score.textContent = ` Your score: ${playerScore}. Computer Score ${computerScore}`;
+    }else{
+        choiceMade.textContent = `You chose ${playerSelection}. Computer chose ${computerSelection}`;
+        roundWinner.textContent = 'It\'s a tie!';
+        score.textContent = ` Your score: ${playerScore}. Computer Score ${computerScore}`;
+    }
+}
 
-    function playRound(playerSelection, computerSelection){
-
-    if (computerSelection === playerSelection ) {
-        return "Its a tie";
-     } else if ((computerSelection === "rock" &&playerSelection    === "scissors")) {
-        computerScore +=1;
-        return "Computer wins. Rock beats Scissors."
-    }else if ((computerSelection === "paper" && playerSelection === "rock")) {
-        computerScore +=1;
-        return "Computer wins. Paper beats Rock";
-    }else if ((computerSelection === "scissors" && playerSelection === "paper")){
-        computerScore +=1;
-        return "Computer wins. Scissors beats Paper";
-    }else if ((computerSelection === "scissors" && playerSelection === "rock")) {
-        playerScore +=1;
-        return "Player wins. Rock beats Scissors.";
-    }else if ((computerSelection ==="rock" && playerSelection === "paper")) {
-        playerScore +=1;
-        return "Player wins. Paper beats Rock";
-    }else if ((computerSelection === "paper" && playerSelection === "scissors")) {
-        playerScore +=1;
-        return (("Player wins. Scissors beats Paper."))
-    }else {
-       return "Something went wrong.Please try again."
-            }
-        }   
-    
-  
+// winner is declared
+function finishedGame() {
+     if(playerScore === 5){
+        gameWinner.textContent = "Congratulations! YOU WIN!";
+         reset();
         
+     }else if (computerScore === 5){
+         gameWinner.textContent = "Computer Wins. Try again?"
+         reset();   
+     }
+};
 
 
+
+
+//reset game
+function reset() {
+    choiceMade.textContent = " ";
+    roundWinner.textContent = "";
+    score.textContent = "";
+    playerScore = 0;
+    computerScore = 0;
+}
+
+// round is played (old code) ----------------------------------
+
+    // function playRound(playerSelection, computerSelection){
+
+    // if (computerSelection === playerSelection ) {
+    //     roundWinner.textContent = 'It\'s a tie!'
+    //  } else if ((computerSelection === "rock" &&playerSelection    === "scissors")) {
+    //     computerScore +=1;
+    //     roundWinner.textContent = `Computer Wins: ${computerSelection} beats ${playerSelection}`;
+    // }else if ((computerSelection === "paper" && playerSelection === "rock")) {
+    //     computerScore +=1;
+    //     roundWinner.textContent = `Computer Wins: ${computerSelection} beats ${playerSelection}`;
+    // }else if ((computerSelection === "scissors" && playerSelection === "paper")){
+    //     computerScore +=1;
+    //     roundWinner.textContent = `Computer Wins: ${computerSelection} beats ${playerSelection}`;
+    // }else if ((computerSelection === "scissors" && playerSelection === "rock")) {
+    //     playerScore +=1;
+    //     roundWinner.textContent = `Player Wins: ${playerSelection} beats ${computerSelection}`;
+    // }else if ((computerSelection ==="rock" && playerSelection === "paper")) {
+    //     playerScore +=1;
+    //     roundWinner.textContent = `Player Wins: ${playerSelection} beats ${computerSelection}`;
+    // }else if ((computerSelection === "paper" && playerSelection === "scissors")) {
+    //     playerScore +=1;
+    //     roundWinner.textContent = `Player Wins: ${playerSelection} beats ${computerSelection}`;
+    // }else {
+    //    return "Something went wrong.Please try again."
+    //         }
+    //     }
